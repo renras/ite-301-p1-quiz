186c65250a62d3e2253d687540eff3a8a1e956b4
Renzo Visperas <renzovisperas07@gmail.com>
2022-08-02 13:21:51 +0800

    add image name and section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
-  return <div>app!</div>;
+  return (
+    <div className="p-5">
+      <div className="d-flex gap-5 align-items-center">
+        <img
+          src="https://api.time.com/wp-content/uploads/2019/11/gettyimages-459761948.jpg"
+          alt="banana"
+          className="img-thumbnail"
+          style={{ aspectRatio: "1 / 1", width: "10rem" }}
+        />
+        <div className="d-flex flex-column">
+          <p>RENZO VISPERAS</p>
+          <p>UP-FA1-BSITWEBDEV3-7</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
